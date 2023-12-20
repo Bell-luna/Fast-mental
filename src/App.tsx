3e814dd635ec5de40392e878f0aea4d4f464eeea
Belen Luna <belen.luna@flat.mx>
2023-12-20 12:18:04 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+type Color = {
+  name: string;
+  color: string;
+  correct: boolean;
+};
+
+const COLORS: Color[] = [
+  {
+    name: "rojo",
+    color: "#f00",
+    correct: false,
+  },
+  {
+    name: "verde",
+    color: "#f00",
+    correct: false,
+  },
+  {
+    name: "amarillo",
+    color: "#0f0",
+    correct: false,
+  },
+  {
+    name: "azul",
+    color: "#00f",
+    correct: false,
+  },
+];
 
 function App() {
+  const [status, setStatus] = useState<"initial" | "playing" | "finished">(
+    "initial"
+  );
+  const [time, setTime] = useState<number>(0);
+  const [score, setScore] = useState<number>(0);
+  const [gameColors, setGameColors] = useState<Color[]>([]);
+  useEffect(() => {
+    let interval: any;
+
+    if (status === "playing") {
+      interval = setInterval(() => {
+        setTime((time) => time + 1);
+      }, 1000);
+    }
+    return () => {
+      clearInterval(interval);
+    };
+  }, [status]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header>
+        <h1>{0} puntos</h1>
+        <h1>{time} segundos</h1>
       </header>
-    </div>
+      {status === "playing" && (
+        <section>
+          <span>Blanco</span>
+        </section>
+      )}
+      <footer>
+        {status === "initial" && (
+          <button onClick={() => setStatus("playing")}>Jugar</button>
+        )}
+        {status === "finished" && (
+          <button onClick={() => setStatus("initial")}>Reiniciar</button>
+        )}
+      </footer>
+    </main>
   );
 }
 
